fbea88d5b04b2bdb24ad6083365f77fc59257741
Yaroslav <47113131+Yaroher2442@users.noreply.github.com>
2021-03-16 17:23:38 +0300

    difficult 1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
 def task(array):
     #list.index
     return list(array).index('0')
-
+#Сложность O(n)
 
 def task_1(array):
     # str.index
     return array.index('0')
-
+#Сложность O(n)
 
 def task_2(array):
     # my for
@@ -15,7 +15,7 @@ def task_2(array):
             return i
         else:
             continue
-
+#Сложность O(n)
 
 def main():
     print(task("111111111111111111000000000000000000"))
